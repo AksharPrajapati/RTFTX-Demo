48eb2e89b14aca1eb368a72f1e0f50fe908f3419
AksharPrajapati <aksharpp369@gmail.com>
2022-06-16 03:01:32 +0530

    add shoes glb model

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { Suspense } from 'react'
 import './App.css';
+import { Canvas} from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei';
+import Shoes from './components/models/Shoes';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Canvas concurrent pixelRatio={[1, 1.5]} camera={{ position: [0, 0, 2.75] }}>
+      <ambientLight intensity={0.3} />
+      <spotLight intensity={0.3} angle={0.1} penumbra={1} position={[5, 25, 20]} />
+      <Suspense fallback={null}>
+        <Shoes />
+      </Suspense>
+      <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
+    </Canvas>
+  </>
+  )
+
 }
 
 export default App;
